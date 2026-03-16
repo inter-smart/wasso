c2e0375482ce0f2601ccc84b8e01ad0e5298b384
Jishnu <jishnu@intersmart.in>
2026-03-16 15:26:00 +0530

    next config upload path for production added + edited

--- next.config.mjs
+++ next.config.mjs
@@ -2,44 +2,43 @@
 const nextConfig = {
   images: {
     remotePatterns: [
-      // ✅ Strapi Production Server
+      // Strapi production images
       {
         protocol: "https",
-        hostname: "https://strapi.intersmart.in",
-        // pathname: "/uploads/**",
+        hostname: "strapi.intersmart.in",
+        pathname: "/uploads/**",
       },
+
       {
         protocol: "https",
         hostname: "images.unsplash.com",
         pathname: "/**",
       },
+
       {
         protocol: "http",
         hostname: "localhost",
-        port: "1337", // optional but good
-        // pathname: "/uploads/**", // ← THIS WAS MISSING
+        port: "1337",
+        pathname: "/uploads/**",
       },
+
       {
         protocol: "https",
         hostname: "picsum.photos",
         pathname: "/**",
       },
     ],
-    // ✅ Allow localhost/private IP images
-    // dangerouslyAllowLocalIP: true,
 
     formats: ["image/avif", "image/webp"],
     deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
     imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
     minimumCacheTTL: 60,
   },
-  // Compress responses
+
   compress: true,
-  // Enable React strict mode
   reactStrictMode: true,
-  // Power optimization
   poweredByHeader: false,
-  // Experimental features for better performance
+
   experimental: {
     optimizePackageImports: ["lucide-react", "@radix-ui/react-slot"],
   },
@@ -51,7 +50,7 @@ const nextConfig = {
         has: [
           {
             type: "host",
-            value: "https://strap.intersmart.in",
+            value: "strap.intersmart.in",
           },
         ],
         destination: "https://strapi.intersmart.in/:path*",
