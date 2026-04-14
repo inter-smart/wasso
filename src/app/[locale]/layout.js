5f7a8f4492fad531e798d1f722fd3c1e50e44382
unknown <jishnu@intersmart.in>
2026-04-14 14:33:01 +0530

    google font preload cdn  removed due to 404 broken link issue .., bug fixing in progress

--- src/app/[locale]/layout.js
+++ src/app/[locale]/layout.js
@@ -122,13 +122,6 @@ export default async function RootLayout({ children, params }) {
       suppressHydrationWarning
     >
       <head>
-        {/* Preconnect to Google Fonts for faster loading */}
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link
-          rel="preconnect"
-          href="https://fonts.gstatic.com"
-          crossOrigin="anonymous"
-        />
         {/* DNS prefetch for external resources */}
         <link rel="dns-prefetch" href="https://images.unsplash.com" />
       </head>
